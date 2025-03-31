ac778a81a1029d90ac01a4ca1273b444745b0887
Maya Lee <maya.v.lee393@gmail.com>
2025-03-31 17:31:21 -0500

    small things

--- src/pages/activities/meditation.js
+++ src/pages/activities/meditation.js
@@ -3,7 +3,7 @@ import React from 'react'
 const meditation = () => {
   return (
     <div>
-      meditation
+      meditation videos
     </div>
   )
 }
--- src/pages/activities/yoga.js
+++ src/pages/activities/yoga.js
@@ -3,7 +3,7 @@ import React from 'react'
 const yoga = () => {
   return (
     <div>
-      yoga
+      yoga soundtracks
     </div>
   )
 }
