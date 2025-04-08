f0ed40d5923e80a4844c15a1739e2e9e78f4e39b
your_imeldaish <your_imeldaish@utexas.edu>
2025-04-08 12:33:54 -0500

    Style navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import { IconHome, IconPlant2, IconTargetArrow, IconBook } from '@tabler/icons-react';
 
 function Navbar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <nav className="navbar">
       <ul className='navbar-nav'>
-        <li className='nav-item ms-3'><NavLink to="/" end>Home</NavLink></li>
-        <li className='nav-item ms-3'><NavLink to="/suggestions">Suggestions</NavLink></li>
-        <li className='nav-item ms-3'><NavLink to="/journalList">Journal List</NavLink></li>
-        <li className='nav-item ms-3'><NavLink to="/journalEntry">Journal Entry</NavLink></li>
+        <li><NavLink to="/" end><IconHome color="#000000" width="32" height="32"/></NavLink></li>
+        <li><NavLink to="/suggestions"><IconPlant2 color="#000000" width="32" height="32"/></NavLink></li>
+        <li><NavLink to="/journalList"><IconTargetArrow color="#000000" width="32" height="32"/></NavLink></li>
+        <li><NavLink to="/journalEntry"><IconBook color="#000000" width="32" height="32"/></NavLink></li>
       </ul>
     </nav>
   );
